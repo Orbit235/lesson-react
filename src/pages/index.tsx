91c553304cd40bf2b7c36d7a391dcf085fa8e057
unknown <v.ionichev@gmail.com>
2022-11-26 13:36:34 +0300

    init

---
+++ src/pages/index.tsx
@@ -0,0 +1,27 @@
+import {
+   Text  
+
+} from '@chakra-ui/react'
+
+import { Hero } from '../components/Hero'
+import { Container } from '../components/Container'
+import { Main } from '../components/Main'
+import { DarkModeSwitch } from '../components/DarkModeSwitch'
+import { Footer } from '../components/Footer'
+
+const Index = () => (
+  <Container height="100vh">
+    <Hero title='Список дел'/>
+    <Main>
+
+     
+    </Main>
+
+    <DarkModeSwitch />
+    <Footer>
+      <Text>Next ❤️ Chakra</Text>
+    </Footer>    
+  </Container>
+)
+
+export default Index
