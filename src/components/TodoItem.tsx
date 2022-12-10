8bdf72af57c2b42b545e4acf4aa634327d93dd8d
= <v.ionichev@gmail.com>
2022-12-10 12:37:32 +0300

    feat: add mock backend

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,34 +1,51 @@
-import { DeleteIcon } from "@chakra-ui/icons";
-import { Center, Checkbox, Flex, IconButton, Text } from "@chakra-ui/react";
-import { FC, useState } from "react";
-import { Todo } from "../types/todo";
-
+import { DeleteIcon } from '@chakra-ui/icons'
+import { Center, Checkbox, Flex, IconButton, Text } from '@chakra-ui/react'
+import { FC, useState } from 'react'
+import { Todo } from '../types/todo'
 
 interface TodoItemProps {
-    serial: number
-    deleteItem: any
-    todo: Todo
+  serial: number
+  deleteItem: any
+  todo: Todo
 }
-const TodoItem: FC <TodoItemProps> = (props) => {
-    const [complete, setComplete] = useState(props.todo.completed)
-    //console.log (props.todo);
-    if(props.todo.isDeleted) {return <></>}
-    
-    return (
-        <Flex rounded={'xl'} border={'1px dashed'} borderColor={'blue.300'} gap={3} p={3}> 
-        <Checkbox isChecked={complete} onChange={() => {setComplete(!complete)}}/>
-        <Text as={complete ? 'del' : 'p'} alignSelf={'center'}>
-            <Text as='span' color={'blackAlpha.300'}> {props.serial} </Text>. {props.todo.title} </Text>
-            <IconButton
-  variant='outline'
-  colorScheme='teal'
-  aria-label='Call Sage'
-  ml={'auto'}
-  icon={<DeleteIcon />}
-  onClick={props.deleteItem}
-/>
-        </Flex>
- )
+const TodoItem: FC<TodoItemProps> = (props) => {
+  const [complete, setComplete] = useState(props.todo.completed)
+  //console.log (props.todo);
+  if (props.todo.isDeleted) {
+    return <></>
+  }
+
+  return (
+    <Flex
+      rounded={'xl'}
+      border={'1px dashed'}
+      borderColor={'blue.300'}
+      gap={3}
+      p={3}
+    >
+      <Checkbox
+        isChecked={complete}
+        onChange={() => {
+          setComplete(!complete)
+        }}
+      />
+      <Text as={complete ? 'del' : 'p'} alignSelf={'center'}>
+        <Text as='span' color={'blackAlpha.300'}>
+          {' '}
+          {props.serial}{' '}
+        </Text>
+        . {props.todo.title}{' '}
+      </Text>
+      <IconButton
+        variant='outline'
+        colorScheme='teal'
+        aria-label='Call Sage'
+        ml={'auto'}
+        icon={<DeleteIcon />}
+        onClick={props.deleteItem}
+      />
+    </Flex>
+  )
 }
 
 export default TodoItem
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,79 +1,101 @@
-import { AddIcon, SmallAddIcon } from "@chakra-ui/icons"
-import { Flex, FormControl, Input, Button, InputGroup, InputRightElement} from "@chakra-ui/react"
-import { useEffect, useState } from "react"
-import { TodoApi } from "../api/todoApi"
-import { Todo } from "../types/todo"
-import TodoItem from "./TodoItem"
+import { AddIcon, SmallAddIcon } from '@chakra-ui/icons'
+import {
+  Flex,
+  FormControl,
+  Input,
+  Button,
+  InputGroup,
+  InputRightElement,
+} from '@chakra-ui/react'
+import { useEffect, useState } from 'react'
+import { TodoApi } from '../api/todoApi'
+import { Todo } from '../types/todo'
+import TodoItem from './TodoItem'
 const TodoList = () => {
-    const [ todos, setTodos ] = useState([] as Todo[])
-    const [ newTodo, setNewTodo ] = useState("")
-    
-    const loadData = () => {
-        TodoApi.getTodos()
-            .then((response) => {
-                    console.log(response.data)
-                    setTodos(response.data.map((item) => ({...item, isDeleted: false})))
-            })
-            .catch ((err) => {console.log(err);
-            })
-    }
+  const [todos, setTodos] = useState([] as Todo[])
+  const [newTodo, setNewTodo] = useState('')
 
-const deleteItem = (i: number) => {
-    console.log ('deleting', i);
-    setTodos(todos.map ((item, idx) => ({...item,
-        isDeleted: idx == i ? true : item.isDeleted})))
-}
+  const loadData = () => {
+    TodoApi.getTodos()
+      .then((response) => {
+        console.log(response.data)
+        setTodos(response.data.map((item) => ({ ...item, isDeleted: false })))
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
 
-const addItem = (value: string) => {
-    console.debug ("adding item", value);
-setTodos ([...todos,
-    {
+  const deleteItem = (i: number) => {
+    console.log('deleting', i)
+    setTodos(
+      todos.map((item, idx) => ({
+        ...item,
+        isDeleted: idx == i ? true : item.isDeleted,
+      }))
+    )
+    TodoApi.deleteTodo(i + 1)
+  }
+
+  const addItem = (todo: Todo) => {
+    console.debug('adding item', todo.title)
+    setTodos([
+      ...todos,
+      {
         userId: 1,
-        title: value,
+        title: todo.title,
         completed: false,
         isDeleted: false,
-    }
-])
-}
+      },
+    ])
+  }
 
+  useEffect(() => {
+    if (todos.length == 0) loadData()
+  }, [])
 
-
-
-
-// useEffect(() => {
-//     loadData()
-// }, [])
-
-    return (
-    <Flex flexDirection= {'column'} gap={5}>
-        <FormControl>
-            <InputGroup>
-            <Input placeholder={"Введите дела"}
+  return (
+    <Flex flexDirection={'column'} gap={5}>
+      <FormControl>
+        <InputGroup>
+          <Input
+            placeholder={'Введите дела'}
             value={newTodo}
             onChange={(event) => {
-            setNewTodo (event.target.value)
-            }
-            }/>
-            <InputRightElement>
+              setNewTodo(event.target.value)
+            }}
+          />
+          <InputRightElement>
             <Button
-                variant={"ghost"}
-                onClick ={() => {
-addItem (newTodo)
-setNewTodo ("")
-                }} 
-                >               
-                <AddIcon/>
+              variant={'ghost'}
+              onClick={() => {
+                const newTodoItem: Todo = {
+                  userId: 1,
+                  title: newTodo,
+                  completed: false,
+                  isDeleted: false,
+                }
+                TodoApi.addTodo(newTodoItem)
+                addItem(newTodoItem)
+                setNewTodo('')
+              }}
+            >
+              <AddIcon />
             </Button>
-            </InputRightElement>
-            </InputGroup>
-          
-        </FormControl>
-        {
-            todos.map((item, idx) => {
-                return <TodoItem todo={item} serial={idx+1} key={idx} deleteItem={() => deleteItem(idx)}/>
-            })
-        }
-       </Flex>
-)
-    }
+          </InputRightElement>
+        </InputGroup>
+      </FormControl>
+      {todos.map((item, idx) => {
+        return (
+          <TodoItem
+            todo={item}
+            serial={idx + 1}
+            key={idx}
+            deleteItem={() => deleteItem(idx)}
+          />
+        )
+      })}
+    </Flex>
+  )
+}
 export default TodoList
